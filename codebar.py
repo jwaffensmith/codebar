419e265b5dea1e82f9f7adc8575f9356bf9bd1b1
Username <jwaffensmith@gmail.com>
2021-08-11 16:15:58 -0700

    created super class

---
+++ codebar.py
@@ -0,0 +1,22 @@
+# PART I
+
+# Super Class 
+class Member:
+    def __init__(self, member_name):
+        self.full_name = member_name
+    
+    def introduce(self):
+        print(f"Hi, my name is {self.full_name}!")
+
+jenna = Member("Jenna Waffensmith")
+print(jenna)
+
+
+#Sub Class
+#class Student(Member):
+
+#Sub Class
+#class Instructor(Member):
+
+#PART 2
+#Workshops
